5d3efe7cc91e826b1efc54846333da2dd0877ea7
Nakaji Kohki <boomerang.cube@gmail.com>
2019-08-15 22:15:22 +0900

    Add all files

---
+++ Assets/ImageFitter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class ImageFitter : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<RawImage>().FixAspect();
+    }
+}
---
+++ Assets/RawImageExtensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public static class FixAspectExtensions
+{
+    public static void FixAspect(this RawImage image)
+    {
+        image.FixAspect(image.rectTransform.rect.size);
+    }
+    public static void FixAspect(this RawImage image, Vector3 originalSize)
+    {
+        var textureSize = new Vector2(image.texture.width, image.texture.height);
+
+        var heightScale = originalSize.y / textureSize.y;
+        var widthScale = originalSize.x / textureSize.x;
+        var rectSize = textureSize * Mathf.Min(heightScale, widthScale);
+
+        var anchorDiff = image.rectTransform.anchorMax - image.rectTransform.anchorMin;
+        var parentSize = (image.transform.parent as RectTransform).rect.size;
+        var anchorSize = parentSize * anchorDiff;
+
+        image.rectTransform.sizeDelta = rectSize - anchorSize;
+    }
+}
