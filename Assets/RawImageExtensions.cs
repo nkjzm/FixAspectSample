96f6ae16091c211bb1aad513d0193920d8b112c2
Nakaji Kohki <boomerang.cube@gmail.com>
2019-08-15 22:57:55 +0900

    Add comment

--- Assets/RawImageExtensions.cs
+++ Assets/RawImageExtensions.cs
@@ -1,12 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// RawImageの大きさを変える拡張メソッド
+/// </summary>
 public static class FixAspectExtensions
 {
+    /// <summary>
+    /// アスペクト比に合わせてRawImageのサイズを修正する
+    /// 現在のUIサイズが基準となる
+    /// </summary>
     public static void FixAspect(this RawImage image)
     {
         image.FixAspect(image.rectTransform.rect.size);
     }
+    /// <summary>
+    /// アスペクト比に合わせてRawImageのサイズを修正する
+    /// </summary>
+    /// <param name="originalSize">基準となるUIサイズ</param>
     public static void FixAspect(this RawImage image, Vector3 originalSize)
     {
         var textureSize = new Vector2(image.texture.width, image.texture.height);
